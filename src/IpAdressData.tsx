32fd592d61518d6a834dce29a25089b18f059f88
khennyboy <abdullateefkehinde848@gmail.com>
2025-07-04 16:03:08 +0100

    done

--- src/IpAdressData.tsx
+++ src/IpAdressData.tsx
@@ -6,7 +6,7 @@ function IPAddressData({ data }: IPData) {
   if (!data || Object.keys(data).length === 0) return null;
 
   return (
-    <div className="md:flex shadow-md w-[90%] [@media(min-width:500px)_and_(max-width:768px)]:w-[70%] max-w-4xl bg-white py-8 absolute rounded-xl items-center *:text-center bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-1000 space-y-4 md:space-y-0 ">
+    <div className="md:flex shadow-md w-[90%] [@media(min-width:500px)_and_(max-width:768px)]:w-[70%] max-w-4xl bg-white py-8 absolute rounded-xl items-center *:text-center bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-1000 space-y-4 md:space-y-0 divide-dark-gray divide-x-2">
       <div className="*:block flex-auto">
         <Text className="text-dark-gray" size="1" weight="bold">
           IP ADDRESS
